49344f9fdb9e1b52b211ee57f675297c55a163f5
Carlo Tolin <carlodominiquetolin@gmail.com>
2023-05-29 20:35:25 +0800

    another approach

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 import cv2
 import streamlit as st
 
-#video_capture = cv2.VideoCapture(0)
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-# Display the camera widget in Streamlit
-stframe = st.empty()
-
-num_cameras = cv2.getNumberOfCameras()
-st.write(num_cameras)
-
-"""
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        break
-
-    # Display the captured frame in Streamlit
-    stframe.image(frame, channels="BGR")
-"""
-# Release the camera
-video_capture.release()
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
