bad4f1a4a35ef3f6c2282b4653f7a21962f28da7
Carlo Tolin <carlodominiquetolin@gmail.com>
2023-05-29 20:22:42 +0800

    testcam

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import cv2
 import streamlit as st
 
-video_capture = cv2.VideoCapture(0)
+
+def list_camera_devices():
+    index = 0
+    while True:
+        cap = cv2.VideoCapture(index)
+        if not cap.read()[0]:
+            break
+        print(f"Camera {index}: OK")
+        cap.release()
+        index += 1
+
+list_camera_devices()
+
+#video_capture = cv2.VideoCapture(0)
 
 # Display the camera widget in Streamlit
 stframe = st.empty()
 
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        break
+#while True:
+    #ret, frame = video_capture.read()
+    #if not ret:
+    #    break
 
     # Display the captured frame in Streamlit
-    stframe.image(frame, channels="BGR")
+    #stframe.image(frame, channels="BGR")
 
 # Release the camera
-video_capture.release()
+#video_capture.release()
