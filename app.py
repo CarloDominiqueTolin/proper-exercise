19666884d571d3f86f9f54d285f135631ee6139e
Carlo Tolin <carlodominiquetolin@gmail.com>
2023-05-29 20:32:56 +0800

    number of cams

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import cv2
 import streamlit as st
 
-video_capture = cv2.VideoCapture(0)
+#video_capture = cv2.VideoCapture(0)
 
 # Display the camera widget in Streamlit
 stframe = st.empty()
 
+num_cameras = cv2.getNumberOfCameras()
+st.write(num_cameras)
+
+"""
 while True:
     ret, frame = video_capture.read()
     if not ret:
@@ -13,6 +17,6 @@ while True:
 
     # Display the captured frame in Streamlit
     stframe.image(frame, channels="BGR")
-
+"""
 # Release the camera
 video_capture.release()
